ff9415e308e672996634d60e0e19a9314a72ee1e
wahyu kharisma mujiono <wahyukharismaz@gmail.com>
2021-07-17 17:49:32 +0700

    fix bg fill

--- lib/ui/MainMenu.dart
+++ lib/ui/MainMenu.dart
@@ -9,59 +9,58 @@ class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final userController = Get.put(UserController());
-    return SafeArea(
-      child: Stack(
-        children: [
-          Image.asset(
-            'assets/bg_validation.png',
-            fit: BoxFit.fill,
-          ),
-          Scaffold(
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Image.asset(
+          'assets/bg_validation.png',
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
             backgroundColor: Colors.transparent,
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              title: Text(''),
-            ),
-            body: Obx(
-              () => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('Nama\t: '),
-                          Expanded(
-                            child: Text(userController.nama.value),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Components.btn(
-                          userController.event.value.isEmpty
-                              ? 'pilih event'
-                              : userController.event.value, () {
-                        Get.to(() => Event());
-                      }),
-                      Components.btn(
-                          userController.guest.value.isEmpty
-                              ? 'pilih guest'
-                              : userController.guest.value, () {
-                        Get.to(() => Guest());
-                      }),
-                    ],
-                  ),
+            elevation: 0,
+            title: Text(''),
+          ),
+          body: Obx(
+            () => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Nama\t: '),
+                        Expanded(
+                          child: Text(userController.nama.value),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Components.btn(
+                        userController.event.value.isEmpty
+                            ? 'pilih event'
+                            : userController.event.value, () {
+                      Get.to(() => Event());
+                    }),
+                    Components.btn(
+                        userController.guest.value.isEmpty
+                            ? 'pilih guest'
+                            : userController.guest.value, () {
+                      Get.to(() => Guest());
+                    }),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
